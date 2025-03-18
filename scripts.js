9346bac5934eff6f4b4a3ced3518dd15b5d57c99
Rodrick-Lee <147120643+Rodrick-Lee@users.noreply.github.com>
2025-03-17 20:28:01 -0600

    Add Event listeners for hover de cells

--- scripts.js
+++ scripts.js
@@ -2,16 +2,36 @@
 
 const container = document.querySelector(".container")
 for (let i = 0; i < 16; i++) {
-    const column = document.createElement("div");
-    column.setAttribute("class", "column")
-    column.setAttribute("id", `column-${i}`)
+    const row = document.createElement("div");
+    row.setAttribute("class", "row")
+    row.setAttribute("id", `row-${i}`)
     for (let j = 0; j < 16; j++) {
-        const rows = document.createElement("div");
-        rows.setAttribute("class", "rows")
-        rows.setAttribute("id", `rows-${i},${j}`)
-        column.appendChild(rows);   
+        const cell = document.createElement("div");
+        cell.setAttribute("class", "cell")
+        cell.setAttribute("id", `cell-${i},${j}`)
+        row.appendChild(cell);   
     }
-    container.appendChild(column);
+    container.appendChild(row);
 }
 
 console.log(container)
+
+
+// function hover(e){
+//     console.log("the mouse leave a div")
+// }
+
+
+const cells = document.querySelectorAll(".cell");
+
+cells.forEach((cell) => {
+    cell.addEventListener("mouseenter", (e) => {
+        cell.classList.add("enter");
+    });
+
+    cell.addEventListener("mouseleave", (e) => {
+        cell.classList.add("leave");
+    });
+});
+
+const btn = document.querySelector(".btn")
