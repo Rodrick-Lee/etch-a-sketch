61d73eb126253b2fa4d4095957ae43948d34ce75
Rodrick-Lee <147120643+Rodrick-Lee@users.noreply.github.com>
2025-03-17 21:40:28 -0600

    finish proyect

--- scripts.js
+++ scripts.js
@@ -1,37 +1,50 @@
-//Creation 16x16 matrix
-
 const container = document.querySelector(".container")
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.setAttribute("class", "row")
-    row.setAttribute("id", `row-${i}`)
-    for (let j = 0; j < 16; j++) {
-        const cell = document.createElement("div");
-        cell.setAttribute("class", "cell")
-        cell.setAttribute("id", `cell-${i},${j}`)
-        row.appendChild(cell);   
+const btn = document.querySelector(".btn")
+
+function createGrid(size) {
+    container.innerHTML = ""
+
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.setAttribute("class", "row")
+        row.setAttribute("id", `row-${i}`)
+        for (let j = 0; j < size; j++) {
+            const cell = document.createElement("div");
+            cell.setAttribute("class", "cell")
+            cell.setAttribute("id", `cell-${i},${j}`)
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
+    const cells = document.querySelectorAll(".cell");
+    const cellSize = 960 / size
+    cells.forEach((cell) => {
+        cell.setAttribute("style", `width: ${cellSize}px; height: ${cellSize}px`)
+        cell.addEventListener("mouseenter", (e) => {
+            cell.classList.add("hover");
+        });
+
+        cell.addEventListener("mouseleave", (e) => {
+            cell.classList.add("leave");
+        });
+    });
 }
 
-console.log(container)
+function resize() {
+    const size = parseInt(prompt("Enter a number of cell per side: ",))
+    if (isNaN(size)) {
+        alert("Please enter a valid number.");
+        resize()
+    }
+    if (size > 100) {
+        alert("The number of cells is too high, please enter another number.")
+        resize()
+    }
+   
+    createGrid(size)
+}
 
+btn.addEventListener("click", resize)
 
-// function hover(e){
-//     console.log("the mouse leave a div")
-// }
-
-
-const cells = document.querySelectorAll(".cell");
-
-cells.forEach((cell) => {
-    cell.addEventListener("mouseenter", (e) => {
-        cell.classList.add("enter");
-    });
-
-    cell.addEventListener("mouseleave", (e) => {
-        cell.classList.add("leave");
-    });
-});
-
-const btn = document.querySelector(".btn")
+const size = 16
+createGrid(size)
